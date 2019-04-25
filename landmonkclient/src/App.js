7e9e9e6ebb32dd19402e43c13b22a6927fcf24a8
Paul Epps <paul@eppsnet.com>
2019-04-25 15:21:58 -0700

    Add routing

--- landmonkclient/src/App.js
+++ landmonkclient/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NotFound from '../components/ErrorPages/NotFound/NotFound';
 
 function App() {
   return (
+    <BrowserRouter>
+      <Switch>
+        Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
